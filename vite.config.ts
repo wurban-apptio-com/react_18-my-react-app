85cd5b8defe06ba9ae4f91167dbc9c6f2416eb78
Wiktor Urban <wurban@apptio.com>
2024-12-23 16:33:38 +0100

    add copyrights

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/*********************************************************************
+ * © Copyright IBM Corp. 2024
+ *********************************************************************/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
