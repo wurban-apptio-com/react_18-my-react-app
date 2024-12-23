ce2263ae452a7bdbca828616dd4227b34a8ef5f9
Wiktor Urban <wurban@apptio.com>
2024-12-23 16:54:49 +0100

    add husky, eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 /*********************************************************************
  * © Copyright IBM Corp. 2024
  *********************************************************************/
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /*********************************************************************
  * © Copyright IBM Corp. 2024
  *********************************************************************/
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
