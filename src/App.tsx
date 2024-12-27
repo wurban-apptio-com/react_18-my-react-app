da340fa7b9c7719c85baf25b51b8b5a38e00ad49
Wiktor Urban <wurban@apptio.com>
2024-12-27 12:30:01 +0100

    add react-carbon and scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
 /*********************************************************************
  * © Copyright IBM Corp. 2024
  *********************************************************************/
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import './App.css';
 import { LazyLoadImport } from './utils';
+import { Add } from '@carbon/react/icons';
 
 const SampleComponent = LazyLoadImport(
   async () => (await import('./components/SampleComponent')).SampleComponent,
 );
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Add />
       <SampleComponent />
     </>
   );
--- src/components/SampleComponent.tsx
+++ src/components/SampleComponent.tsx
@@ -1,10 +1,14 @@
 /*********************************************************************
  * © Copyright IBM Corp. 2024
  *********************************************************************/
+
+import { Button } from '@carbon/react';
+
 export function SampleComponent() {
   return (
     <div>
       <div>Hello from SampleComponent</div>
+      <Button type="button">Click me</Button>
     </div>
   );
   // return <>Hello from SampleComponent</>;
